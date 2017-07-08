af2502d131d6d5a74265e21161a653bb788a3e5c
Hanley Weng <hanleyweng@gmail.com>
2017-07-08 23:09:51 +1000

    Added left and right eye SceneViews. Turned them on.

--- ARKit Headset View/ViewController.swift
+++ ARKit Headset View/ViewController.swift
@@ -13,21 +13,33 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var sceneViewLeft: ARSCNView!
+    @IBOutlet weak var sceneViewRight: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
         // Set the scene to the view
         sceneView.scene = scene
+        
+        ////////////////////////////////////////////////////////////////
+        // Set up Left-Eye SceneView
+        sceneViewLeft.scene = scene
+        sceneViewLeft.showsStatistics = sceneView.showsStatistics
+        sceneViewLeft.isPlaying = true
+        
+        // Set up Right-Eye SceneView
+        sceneViewRight.scene = scene
+        sceneViewRight.showsStatistics = sceneView.showsStatistics
+        sceneViewRight.isPlaying = true
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
